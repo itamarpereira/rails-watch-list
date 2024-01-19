77314925ee14386f61ba866281fd2954cbfea756
Itamar Pereira <itamartpereira@hotmail.com>
2024-01-19 00:24:23 -0300

    Drop, Create and Migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,13 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_19_011103) do
   enable_extension "plpgsql"
 
   create_table "bookmarks", force: :cascade do |t|
-    t.string "comment"
+    t.text "comment"
+    t.bigint "movie_id", null: false
+    t.bigint "list_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
   end
 
   create_table "lists", force: :cascade do |t|
@@ -35,4 +39,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_19_011103) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
