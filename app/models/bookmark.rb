9ba9f1895a19bf0d861bc9b28c8a53cd131769f0
Itamar Pereira <itamartpereira@hotmail.com>
2024-01-18 23:40:10 -0300

    Fixed models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,8 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates_absence_of :comment
-  validates :comment, length: {minimum: 6}    
+  validates_presence_of :movie_id, :list_id
+  validates :comment, length: { minimum: 6 }    
   validates :movie, uniqueness: { scope: :list, message: "Movie & List pairings should be unique" }
 
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks, -> { where active: true },dependent: :destroy
+  has_many :bookmarks, -> { where active: true }, dependent: :destroy
 
   validates :title, presence: true
   validates :overview, presence: true
