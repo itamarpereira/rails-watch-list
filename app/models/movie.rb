a60c5c441bd849bba721548de6a8da913912d406
Itamar Pereira <itamartpereira@hotmail.com>
2024-01-18 23:05:20 -0300

    Fixed movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  has_many :bookmarks, -> { where active: true },dependent: :destroy
 
+  validates :title, presence: true
+  validates :overview, presence: true
   validates :title, uniqueness: true
   validates :overview, uniqueness: true
 end
