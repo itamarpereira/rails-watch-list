6169a41feff5181798e146940cd1f561e6f36c43
Itamar Pereira <itamartpereira@hotmail.com>
2024-01-18 22:11:36 -0300

    Created bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_19_005905) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_19_011103) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: :cascade do |t|
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "lists", force: :cascade do |t|
     t.string "name"
