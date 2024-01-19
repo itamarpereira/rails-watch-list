7278baf2348a05325eb3d12b890c0b4bca5719be
Itamar Pereira <itamartpereira@hotmail.com>
2024-01-18 22:36:32 -0300

    Updated models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,9 +1,9 @@
 class Bookmark < ApplicationRecord
-    belongs_to :movie
-    belongs_to :list
+  belongs_to :movie
+  belongs_to :list
 
-    validates_absence_of :comment
-    validates :comment, length: {minimum: 6}    
-    validates :movie, uniqueness: { scope: :list, message: "Movie & List pairings should be unique" }
+  validates_absence_of :comment
+  validates :comment, length: {minimum: 6}    
+  validates :movie, uniqueness: { scope: :list, message: "Movie & List pairings should be unique" }
 
 end
