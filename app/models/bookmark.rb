3d97117506f997d0110dcc3029812d9845c42a45
Itamar Pereira <itamartpereira@hotmail.com>
2024-01-18 23:55:18 -0300

    Fixed models

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,8 +2,7 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
-  validates_presence_of :movie_id, :list_id
   validates :comment, length: { minimum: 6 }    
-  validates :movie, uniqueness: { scope: :list, message: "Movie & List pairings should be unique" }
+  validates :movie, presence: true
 
 end
